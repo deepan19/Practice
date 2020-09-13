09edaadc4113d79ace7e1c5e2b66cea3ff21bbb8
Deepan Chakravarthy <deepanchakravarthy@host235-128.resnet.ubc.ca>
2020-09-13 15:14:29 -0700

    added method

--- src/main/java/cpen221practice/ForYouAndMe.java
+++ src/main/java/cpen221practice/ForYouAndMe.java
@@ -1,7 +1,19 @@
 package cpen221practice;
 
+
 public class ForYouAndMe {
     public String youandme(String name) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        String str = new String("One for you, one for me.");
+
+        if(name==null){
+            return str;
+        }
+        else{
+            str=str.replace("you",name);
+            return str;
+
+
+
+        }
     }
 }
